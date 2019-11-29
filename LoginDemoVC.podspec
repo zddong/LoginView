b6bf694e3bd198895c4cd6933230e5c6edd10326
850853654@qq.com <850853654@qq.com>
2019-11-29 14:30:09 +0800

    0.1.5

--- LoginDemoVC.podspec
+++ LoginDemoVC.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "LoginDemoVC"
-  s.version      = "0.1.4"
+  s.version      = "0.1.5"
   s.summary      = "LoginDemoVC"
   s.description  = <<-DESC
   LoginDemoVC
---
+++ LoginDemoVC/AuthViewController.h
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.h
+//  LoginDemo
+//
+//  Created by zhao on 2019/11/27.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AuthViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
